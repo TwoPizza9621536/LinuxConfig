5ed7d99df959f192355229a17686be786f5d1c47
Sam W <twopizza9621536@gmail.com>
2021-05-15 18:18:42 -0400

    Update Zsh.zshrc and fedorainstallscript.sh

--- Zsh.zshrc
+++ Zsh.zshrc
@@ -202,4 +202,27 @@ export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+SSH_ENV="$HOME/.ssh/agent-environment"
+
+function start_agent {
+    echo "Initialising new SSH agent..."
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    echo succeeded
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add;
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "${SSH_ENV}" ]; then
+    . "${SSH_ENV}" > /dev/null
+    #ps ${SSH_AGENT_PID} doesn't work under cywgin
+    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
+
 neofetch
--- fedorainstallscript.sh
+++ fedorainstallscript.sh
@@ -19,14 +19,18 @@ echo 'max_parallel_downloads=10' | tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | tee -a /etc/dnf/dnf.conf
 
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo
-curl https://packages.microsoft.com/config/fedora/34/prod.repo | tee /etc/yum.repos.d/microsoft-prod.repo
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee \
+/etc/yum.repos.d/vscode.repo
+curl https://packages.microsoft.com/config/fedora/34/prod.repo | tee \
+/etc/yum.repos.d/microsoft-prod.repo
 
 rpm --import https://www.virtualbox.org/download/oracle_vbox.asc
-curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | tee /etc/yum.repos.d/virtualbox.repo
+curl https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo | tee \
+/etc/yum.repos.d/virtualbox.repo
 
 rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo
+curl https://download.mono-project.com/repo/centos8-stable.repo | tee \
+/etc/yum.repos.d/mono-centos8-stable.repo
 
 curl https://cli.github.com/packages/rpm/gh-cli.repo | tee /etc/yum.repos.d/gh-cli.repo
 
@@ -35,15 +39,28 @@ curl https://www.scala-sbt.org/sbt-rpm.repo | tee /etc/yum.repos.d/sbt-rpm.repo
 
 dnf check-update
 
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 dnf install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted -y
 
 dnf groupupdate core -y
 
-dnf group install --with-optional "Administration Tools" "Audio Production" "Multimedia" "Authoring and Publishing" "C Development Tools and Libraries" "Container Management" "D Development Tools and Libraries" "Design Suite" "Development Tools" "Fedora Eclipse" "Editors" "Educational Software" "LibreOffice" "Office/Productivity" "Python Classroom" "Python Science" "Text-based Internet" "Window Managers" "GNOME Desktop Environment" "Graphical Internet" "KDE (K Desktop Environment)" "Fonts" "Games and Entertainment" "Sound and Video" "System Tools" "KDE Plasma Workspaces" "Minimal Install" "Development and Creative Workstation" "Basic Desktop" "Fedora Workstation" "Xfce Desktop" "Fedora Custom Operating System" --best --allowerasing --skip-broken -y
+dnf group install --with-optional "Administration Tools" "Audio Production" "Multimedia" "Authoring and Publishing" \
+"C Development Tools and Libraries" "Container Management" "D Development Tools and Libraries" "Design Suite" \
+"Development Tools" "Fedora Eclipse" "Editors" "Educational Software" "LibreOffice" "Office/Productivity" \
+"Python Classroom" "Python Science" "Text-based Internet" "Window Managers" "GNOME Desktop Environment" \
+"Graphical Internet" "KDE (K Desktop Environment)" "Fonts" "Games and Entertainment" "Sound and Video" \
+"System Tools" "KDE Plasma Workspaces" "Minimal Install" "Development and Creative Workstation" "Basic Desktop" \
+"Fedora Workstation" "Xfce Desktop" "Fedora Custom Operating System" --best --allowerasing --skip-broken -y
 
-dnf install optipng keybinder* guake* glade* gh hub *apache* sbt *7z* xz* gzip* gedit* geany* bzip2* brotli* lzop* zstd* fzf* vtk* maven* ant* *java* code google-*-fonts fira-code-fonts cascadia-* jetbrains-mono-* neofetch htop virtualbox* gparted vlc mpv gimp* haroopad ffmpeg* fftw* gstreamer* timeshift* lame* cmake* mono-complete mono-basic mono-addins gdb* *keyring* dotnet5* dotnet-sdk* gtk* wx* ruby-* SDL-* glfw-* allegro-* llvm* clang* mpfr-* cloog-* isl-* tex* make bison gmp-* libmpc-* flex ccache R-* qt* kf* kde* mingw* node* python2 python3 pip* npm gcc* ldc ghc* tex* gnome* coreutil* binutil* zsh* dnf* qemu* mscore* pipy* --best --allowerasing --skip-broken -y
+dnf install optipng keybinder* guake* glade* gh hub *apache* sbt *7z* xz* gzip* gedit* geany* bzip2* brotli* lzop* \
+zstd* fzf* vtk* maven* ant* *java* code google-*-fonts fira-code-fonts cascadia-* jetbrains-mono-* neofetch htop \
+virtualbox* gparted vlc mpv gimp* haroopad ffmpeg* fftw* gstreamer* timeshift* lame* cmake* mono-complete \
+mono-basic mono-addins gdb* *keyring* dotnet5* dotnet-sdk* gtk* wx* ruby-* SDL-* glfw-* allegro-* llvm* clang* \
+mpfr-* cloog-* isl-* tex* make bison gmp-* libmpc-* flex ccache R-* qt* kf* kde* mingw* node* python2 python3 pip* \
+npm gcc* ldc ghc* tex* gnome* coreutil* binutil* zsh* dnf* qemu* mscore* pipy* \
+*kleo* --best --allowerasing --skip-broken -y
 
 rpm --import https://repos.codelite.org/CodeLite.asc
 rpm -Uvh https://repos.codelite.org/rpms-15.0/fedora/33/codelite-15.0.1-1.fc33.x86_64.rpm
