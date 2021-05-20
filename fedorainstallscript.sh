27c87e9166ec77b636a3359ee35bcc1069441713
Sam W <twopizza9621536@gmail.com>
2021-05-20 11:44:16 -0400

    Update fedorainstallscript.sh

--- fedorainstallscript.sh
+++ fedorainstallscript.sh
@@ -25,7 +25,8 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 cat << EOF | tee /etc/yum.repos.d/vscode.repo
 [code]
 name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 
